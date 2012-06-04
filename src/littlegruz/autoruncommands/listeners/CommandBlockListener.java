bbca997fc789a186ed458ccd2d296b70001cd310
unknown <littlegruz@gmail.com>
2012-06-04 19:15:18 +1000

    Command blocks can only be removed by an OP

--- src/littlegruz/autoruncommands/listeners/CommandBlockListener.java
+++ src/littlegruz/autoruncommands/listeners/CommandBlockListener.java
@@ -17,7 +17,13 @@ public class CommandBlockListener implements Listener {
    @EventHandler
    public void onBlockBreak(BlockBreakEvent event){
       if(plugin.getBlockCommandMap().get(event.getBlock().getLocation()) != null){
-         plugin.getBlockCommandMap().remove(event.getBlock().getLocation());
+         // Only remove a command block if the player is op
+         if(event.getPlayer().hasPermission("autoruncommands.removeblock"))
+            plugin.getBlockCommandMap().remove(event.getBlock().getLocation());
+         else{
+            event.getPlayer().sendMessage("You don't have permission to remove this block");
+            event.setCancelled(true);
+         }
       }
    }
 }
